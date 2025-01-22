340124ec39eab0b749176fa89da3d36935d6cb7d
joel-kalema <joelkalema63@gmail.com>
2025-01-22 02:00:05 +0200

    updeted the contend

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,6 +1,9 @@
 import React, { useMemo } from "react";
 import dynamic from "next/dynamic";
+import Image from "next/image";
 import CarouselCustomNavigation from "@/components/reuseble/caroucelle";
+import { IoCheckmarkDoneOutline } from "react-icons/io5";
+
 const icons = {
   Tiktok: dynamic(() => import("react-icons/pi").then(mod => mod.PiTiktokLogoLight)),
   Instagram: dynamic(() => import("react-icons/pi").then(mod => mod.PiInstagramLogoLight)),
@@ -31,7 +34,9 @@ const Services = () => {
         <p>We bring your vision to life with:</p>
       </div>
 
-      <div className="w-[10rem] h-[12rem] lg:w-[30rem] lg:h-[40rem] bg_1 rounded-full blur-3xl absolute z-10 top-[40%] right-0 opacity-40"></div>
+      <div className="w-[10rem] h-[12rem] lg:w-[30rem] lg:h-[40rem] bg_1 rounded-full blur-3xl absolute z-10 top-[10%] right-0 opacity-40"></div>
+      <div className="w-[10rem] h-[12rem] lg:w-[30rem] lg:h-[40rem] bg_1 rounded-full blur-3xl absolute z-10 bottom-[10%] left-0 opacity-40"></div>
+
       <div className="w-full h-20"></div>
       <div className="w-5/6 border-x mx-auto h-full relative z-20">
         <div className="border-t -py-2">
@@ -45,7 +50,7 @@ const Services = () => {
             </div>
           </div>
           <div className="h-10 lg:h-20 border-b"></div>
-          <div className="lg:flex h-40 lg:h-80 lg:px-4">
+          <div className="lg:flex h-40 lg:h-80 lg:px-4 bg-white">
             <div className="h-full relative lg:w-3/5 border-l">
               <CarouselCustomNavigation
                 images={["/belekeweb.png", "/beleke4.png", "/qavah.png", "/belekeweb.png"]}
@@ -75,17 +80,57 @@ const Services = () => {
           </div>
         </div>
 
+        <div className="flex my-20 border-y">
+          <div className="w-1/2 px-10 py-14">
+          <h1>Boost your brand with dynamic and captivating content:</h1>
+          <ul  className="pl-5 mt-6">
+            <li className="flex items-center gap-3"><IoCheckmarkDoneOutline className="text-[#FE8C12]"/>Professional YouTube thumbnails</li>
+            <li className="flex items-center gap-3"><IoCheckmarkDoneOutline className="text-[#FE8C12]"/>Engaging Social Media Posts</li>
+            <li className="flex items-center gap-3"><IoCheckmarkDoneOutline className="text-[#FE8C12]"/>Short Promotional Videos</li>
+            <li className="flex items-center gap-3"><IoCheckmarkDoneOutline className="text-[#FE8C12]"/>Eye-Catching Graphics</li>
+            <li  className="flex items-center gap-3"><IoCheckmarkDoneOutline className="text-[#FE8C12]"/>Custom Content Strategies</li>
+          </ul>
+          </div>
+          <div className="h-[50vh] w-1/2 hidden lg:block overflow-hidden relative">
+            <Image src="/content.jpg" layout="fill" objectFit="cover" alt="bay" quality={100} priority />
+          </div>
+        </div>
+
 
         <div className="border-y lg:flex gap-10">
-          <p className="text-xs lg:text-lg font-bold border-t-4 border-[#FE8C12] text-[#FE8C12] uppercase w-1/5">Social Media</p>
+          <p className="text-xs lg:text-lg font-bold border-t-4 border-[#FE8C12] text-[#FE8C12] uppercase">Elevation</p>
           <div>
-            <h1 className="w-5/6 text-md lg:text-4xl font-bold mb-4 lg:mb-10">Top Social media platforms for marketing</h1>
+            <h1 className="text-md lg:text-4xl font-bold mb-4 lg:mb-10">Digital Strategy & Business Consulting</h1>
             <p className="lg:w-4/6">
+            Targeted ad campaigns and tailored growth strategies for businesses and churches to maximize reach and impact.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex my-20 border-y bg-white">
+          <div className="h-[50vh] w-1/2 hidden lg:block overflow-hidden relative">
+            <Image src="/analitics.jpg" layout="fill" objectFit="cover" alt="bay" quality={100} priority />
+          </div>
+
+          <div className="w-1/2 px-10 py-14">
+            <h1>Empower your brand with impactful, data-driven strategies:</h1>
+            <ul className="pl-5 mt-6">
+              <li className="flex items-center gap-3"><IoCheckmarkDoneOutline className="text-[#FE8C12]"/>Targeted Digital Ad Campaigns</li>
+              <li className="flex items-center gap-3"><IoCheckmarkDoneOutline className="text-[#FE8C12]"/>Growth Strategy for Businesses & Churches</li>
+              <li className="flex items-center gap-3"><IoCheckmarkDoneOutline className="text-[#FE8C12]"/>Brand Identity & Messaging Consultation</li>
+              <li className="flex items-center gap-3"><IoCheckmarkDoneOutline className="text-[#FE8C12]"/>Market Trend Analysis & Insights</li>
+          </ul>
+          </div>
+        </div>
+
+
+        <div className="border-y">
+            <h1 className="w-5/6 text-md lg:text-4xl font-bold mb-4 lg:mb-10">Top Social media platforms for marketing</h1>
+            {/* <p className="lg:w-4/6">
               Zion Reach is dedicated to providing creative communication services that make a difference.
               We help our clients grow through professional content creation, strategic marketing,
               and building authentic connections with their audiences.
-            </p>
-          </div>
+            </p> */}
         </div>
 
 
